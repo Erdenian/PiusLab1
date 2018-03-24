559016d670dfdd09f25f4a9e140ed345374f8c8f
Ilya Solovyev <mail@erdenian.ru>
2018-03-24 13:56:45 +0300

    Добавлены скрипты для разбиения бруска на большее количество точек и смещения этих точек с помощью коллайдеров

---
+++ Assets/Scripts/CubeDivider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CubeDivider : MonoBehaviour
+{
+
+    private MeshFilter meshFilter;
+    private Vector3[] vertices;
+
+    public bool test = true;
+    
+    void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        Mesh mesh = meshFilter.mesh;
+        MeshHelper.Subdivide(mesh, 50);
+        meshFilter.mesh = mesh;
+
+        vertices = mesh.vertices;
+    }
+    
+    void Update()
+    {
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            RaycastHit hit;
+            Ray ray = new Ray(vertices[i] + Vector3.down, Vector3.up);
+            if (Physics.Raycast(ray, out hit, 1.0f))
+            {
+                vertices[i] = (test) ? hit.point : hit.point + Vector3.down * 0.001f;
+            }
+        }
+        meshFilter.mesh.vertices = vertices;
+    }
+}

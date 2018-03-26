caf9d21feb30212d9f16ec18de72c7cb40d44365
Pavel Lavrikov <plavrikov@termt.com>
2018-03-26 17:42:36 +0300

    Комментарии и обработка неверных данных и ресет

--- Assets/Scripts/NetworkHandler.cs
+++ Assets/Scripts/NetworkHandler.cs
@@ -12,68 +12,53 @@ using UnityEngine.UI;
 public class NetworkHandler : MonoBehaviour
 {
 
-	private string mess = "test";
 	public int port = 1488;
 
-	private string js = "{" +
-	                    "\"reset\": false," +
-	                    "\"blockSize\": {\"x\": 1.0,\"y\": 1.0,\"z\": 1.0}," +
-	                    "\"knifeSize\": {\"x\": 1.0,\"y\": 1.0,\"z\": 1.0}," +
-	                    "\"points\": [" +
-	                    "{\"x\": 1.0,\"y\": 1.0,\"z\": 1.0}," +
-	                    "{\"x\": 1.0,\"y\": 1.0,\"z\": 1.0}," +
-	                    "{\"x\": 1.0,\"y\": 1.0,\"z\": 1.0}" +
-	                    "]" +
-	                    "}";
-
-
+	private Data data;
 	private TcpListener tcpListener;
 	private Thread tcpListenerThread;
 	private TcpClient connectedTcpClient;
 
 	void Awake ()
-	{	
-		//Debug.Log("1");
-		tcpListenerThread = new Thread (new ThreadStart (ListenForIncommingRequests)); 	
-		//Debug.Log("2");
-		tcpListenerThread.IsBackground = true; 		
-		//Debug.Log("3");
-		tcpListenerThread.Start (); 	
-		//Debug.Log("4");
-
-		Data d = new Data ();
-		d.reset = true;
-		d.blockSize = Vector3.up;
-		d.knifeSize = Vector3.left;
-		d.points = new Vector3[] { Vector3.right, Vector3.forward };
-
-		string jjj = JsonUtility.ToJson (d);
-		Debug.Log (jjj);
-
-
-		Data data = (Data)JsonUtility.FromJson<Data> (jjj);
+	{
+		string text = "{\nd\"reset\": true,\n\"blockSize\":{\"x\":0.0,\"y\":1.0,\"z\":0.0},\n\"knifeSize\":{\"x\":-1.0,\"y\":0.0,\"z\":0.0},\n\"points\":[\n{\"x\":1.0,\"y\":0.0,\"z\":0.0},\n{\"x\":0.0,\"y\":0.0,\"z\":1.0}\n]\n}";
+		Data data = (Data)JsonUtility.FromJson<Data> (text);// разбор полученного сообщения и инициализация объекта
 		Debug.Log (data.blockSize);
+
+		// создаем параллельный поток для получения сообщений клиента
+		tcpListenerThread = new Thread (new ThreadStart (ListenForIncommingRequests));  
+		// делаем поток фоновым
+		tcpListenerThread.IsBackground = true;
+		// запускаем поток обработки
+		tcpListenerThread.Start ();
 	}
 
 	void Update ()
 	{ 		
-		if (Input.GetKeyDown (KeyCode.Space)) {             
-			//SendMessage();  
-			Debug.Log ("ИБУ Я КАКТО ВАШУ МАТ");
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			//SendMessage();
 		} 	
 	}
 
+	//Получение данных для работы стэнда
+	public Data GetData(){
+		return data;
+	}
+
+	// поток обработки входящих сообщений
 	private void ListenForIncommingRequests ()
 	{ 		
-		try {		
+		try {
+			// запускаем сервер
 			tcpListener = new TcpListener (IPAddress.Any, port); 			
 			tcpListener.Start ();              
-			Debug.Log ("КСТА СЛУШАЮ КАРОЧ ХУИТЫ ГОРСТЬ...");              
-			Byte[] bytes = new Byte[1024];  			
+			Debug.Log ("Ожидаем сообщения...");              
+			Byte[] bytes = new Byte[1024];  
+
+			//обработка сообщений
 			while (true) {
 				string clientMessage = "";
-				using (connectedTcpClient = tcpListener.AcceptTcpClient ()) {	
-					Debug.Log ("adsf");
+				using (connectedTcpClient = tcpListener.AcceptTcpClient ()) {
 					using (NetworkStream stream = connectedTcpClient.GetStream ()) { 						
 						int length;  						
 						while ((length = stream.Read (bytes, 0, bytes.Length)) != 0) { 							
@@ -81,50 +66,47 @@ public class NetworkHandler : MonoBehaviour
 							Array.Copy (bytes, 0, incommingData, 0, length);							
 							clientMessage += Encoding.UTF8.GetString (incommingData);
 						}
-						Debug.Log ("ХУИТЫ ПОЛУЧИЛ КАРОЧ Э: " + clientMessage); 
-						mess = clientMessage;
+						Debug.Log ("Получено сообщение: " + clientMessage); 
+						try{
+							data = (Data)JsonUtility.FromJson<Data> (clientMessage);// разбор полученного сообщения и инициализация объекта
+							TableController.Reset();
+						}catch(Exception ex){
+							Debug.Log ("Json поврежден. Данные не получены");
+						}
 					} 				
 				} 			
 			} 		
 		} catch (SocketException socketException) { 			
-			Debug.Log ("ИБАТЬ ТВОЙ РОТ!: " + socketException.ToString ()); 		
+			Debug.Log ("Ошибка: " + socketException.ToString ()); 		
 		}     
 	}
 
-	/// <summary> 	
-	/// Send message to client using socket connection. 	
-	/// </summary> 	
 	private void SendMessage ()
 	{ 		
 		if (connectedTcpClient == null) {             
 			return;         
 		}  		
 
-		try { 			
-			// Get a stream object for writing. 			
+		try {			
 			NetworkStream stream = connectedTcpClient.GetStream (); 			
 			if (stream.CanWrite) {                 
-				string serverMessage = "This is a message from your server."; 			
-				// Convert string message to byte array.                 
-				byte[] serverMessageAsByteArray = Encoding.ASCII.GetBytes (serverMessage); 				
-				// Write byte array to socketConnection stream.               
-				stream.Write (serverMessageAsByteArray, 0, serverMessageAsByteArray.Length);               
-				Debug.Log ("Server sent his message - should be received by client");           
+				string serverMessage = "Ответ сервера";                 
+				byte[] serverMessageAsByteArray = Encoding.UTF8.GetBytes (serverMessage);
+				stream.Write(serverMessageAsByteArray, 0, serverMessageAsByteArray.Length); 
+				Debug.Log("Сервер отправил сообщение клиенту");
 			}       
 		} catch (SocketException socketException) {             
-			Debug.Log ("ИБАТЬ ТВОЙ РОТ!!: " + socketException);         
+			Debug.Log ("Ошибка: " + socketException);         
 		} 	
 	}
 }
 
+//данные
 [Serializable]
 public class Data
 {
 	public bool reset;
-
-	public Vector3 blockSize;
-
-	public Vector3 knifeSize;
-
-	public Vector3[] points;
+	public Vector3 blockSize; // размеры бруска
+	public Vector3 knifeSize; // размеры ножа
+	public Vector3[] points; // точки траектории движения
 }
